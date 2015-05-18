d92e0a2e4d8638279454ce77049308695c736827
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-05-18 03:40:50 +0200

    changed DAO interface methods name

--- src/main/java/de/rwth/i9/palm/persistence/WidgetDAO.java
+++ src/main/java/de/rwth/i9/palm/persistence/WidgetDAO.java
@@ -1,8 +1,25 @@
 package de.rwth.i9.palm.persistence;
 
+import java.util.List;
+
 import de.rwth.i9.palm.model.Widget;
+import de.rwth.i9.palm.model.WidgetType;
 
 public interface WidgetDAO extends GenericDAO<Widget>, InstantiableDAO
 {
+	/**
+	 * Get list of specific widgets by their type
+	 * 
+	 * @param widgetType
+	 * @return
+	 */
+	List<Widget> getActiveWidgetByWidgetType( WidgetType widgetType );
 
+	/**
+	 * Get list of specific widgets by their type and group
+	 * 
+	 * @param widgetType
+	 * @return
+	 */
+	List<Widget> getActiveWidgetByWidgetTypeAndGroup( WidgetType widgetType, String widgetGroup );
 }
