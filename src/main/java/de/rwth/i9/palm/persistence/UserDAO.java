e32913b4780f4cd1cbdedfa0a64c963e9963b80e
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-05-21 05:26:21 +0200

    added User DAO methods

--- src/main/java/de/rwth/i9/palm/persistence/UserDAO.java
+++ src/main/java/de/rwth/i9/palm/persistence/UserDAO.java
@@ -4,5 +4,18 @@ import de.rwth.i9.palm.model.User;
 
 public interface UserDAO extends GenericDAO<User>, InstantiableDAO
 {
+	/**
+	 * @param user
+	 * @param functionName
+	 * @return
+	 */
+	public boolean isAuthorizedForFunction( final User user, final String functionName );
 
+	/**
+	 * Get user object by username
+	 * 
+	 * @param username
+	 * @return
+	 */
+	public User getByUsername( final String username );
 }
