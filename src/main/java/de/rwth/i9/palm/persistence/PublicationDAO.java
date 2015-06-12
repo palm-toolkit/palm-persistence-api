c63dc7a359b5a4cbb6c484f9a49cb61c79643b9f
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-06-12 21:07:17 +0200

    add search by lucene full text on publication DAO and paging functionality

--- src/main/java/de/rwth/i9/palm/persistence/PublicationDAO.java
+++ src/main/java/de/rwth/i9/palm/persistence/PublicationDAO.java
@@ -1,8 +1,43 @@
 package de.rwth.i9.palm.persistence;
 
+import java.util.List;
+import java.util.Map;
+
 import de.rwth.i9.palm.model.Publication;
 
 public interface PublicationDAO extends GenericDAO<Publication>, InstantiableDAO
 {
+	/**
+	 * Trigger batch indexing using Hibernate search powered by Lucene
+	 * 
+	 * @throws InterruptedException
+	 */
+	public void doReindexing() throws InterruptedException;
 
+	/**
+	 * Get all publication in pagination
+	 * 
+	 * @param pageNo
+	 * @param maxResult
+	 * @return
+	 */
+	public Map<String, Object> getPublicationWithPaging( int pageNo, int maxResult );
+
+	/**
+	 * Apply fulltext searh with hibernate search
+	 * 
+	 * @param queryString
+	 * @return list of all related publication
+	 */
+	public List<Publication> getPublicationByFUllTextSearch( String queryString );
+	
+	/**
+	 * Apply fulltext searh with hibernate search with pagging
+	 * 
+	 * @param queryString
+	 * @return Map<String, Object><br/>
+	 * 	Map< "count", Int numberOfMatchingPublication > -  the total number of matching publications <br/>
+	 *  Map< "result", List<Publication> > -  the publication list
+	 */
+	public Map<String, Object> getPublicationByFUllTextSearchWithPaging( String queryString, int pageNo, int maxResult);
 }
