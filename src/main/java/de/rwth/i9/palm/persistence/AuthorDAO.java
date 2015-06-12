1e0d385070c60a958fbb4338f2cbdc32cf26fce8
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-06-12 21:06:52 +0200

    add search by lucene full text on author DAO

--- src/main/java/de/rwth/i9/palm/persistence/AuthorDAO.java
+++ src/main/java/de/rwth/i9/palm/persistence/AuthorDAO.java
@@ -1,5 +1,7 @@
 package de.rwth.i9.palm.persistence;
 
+import java.util.List;
+
 import de.rwth.i9.palm.model.Author;
 
 public interface AuthorDAO extends GenericDAO<Author>, InstantiableDAO
@@ -8,4 +10,12 @@ public interface AuthorDAO extends GenericDAO<Author>, InstantiableDAO
 	public Author getByLastName( String lastName );
 
 	public Author getByUri( String uri );
+
+	/**
+	 * Apply fulltext searh with hibernate search
+	 * 
+	 * @param queryString
+	 * @return
+	 */
+	public List<Author> getAuthorByFUllTextSearch( String queryString );
 }
