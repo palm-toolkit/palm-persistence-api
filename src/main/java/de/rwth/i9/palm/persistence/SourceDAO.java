90c741a5d218d76562ab25ac6f38d3cfe7b1105d
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-08-16 09:24:38 +0200

    add: interface Map getActiveSourceMap on SourceDAO

--- src/main/java/de/rwth/i9/palm/persistence/SourceDAO.java
+++ src/main/java/de/rwth/i9/palm/persistence/SourceDAO.java
@@ -1,8 +1,11 @@
 package de.rwth.i9.palm.persistence;
 
+import java.util.Map;
+
 import de.rwth.i9.palm.model.Source;
+import de.rwth.i9.palm.model.SourceType;
 
 public interface SourceDAO extends GenericDAO<Source>, InstantiableDAO
 {
-
+	public Map<SourceType, Boolean> getActiveSourceMap();
 }
