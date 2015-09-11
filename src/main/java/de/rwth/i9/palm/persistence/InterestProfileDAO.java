fdffd5da1e70f20c7c2a50c5853e8df41161594e
Sigit Nugraha <sigit.nugraha@gesis.org>
2015-09-11 16:13:00 +0200

    Add: getValidInterestProfiles DAO interface

--- src/main/java/de/rwth/i9/palm/persistence/InterestProfileDAO.java
+++ src/main/java/de/rwth/i9/palm/persistence/InterestProfileDAO.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 import de.rwth.i9.palm.model.InterestProfile;
+import de.rwth.i9.palm.model.InterestProfileType;
 
 public interface InterestProfileDAO extends GenericDAO<InterestProfile>, InstantiableDAO
 {
-	public List<InterestProfile> getAllInterestProfile();
+	public List<InterestProfile> getAllValidInterestProfile();
+
+	public List<InterestProfile> getAllValidInterestProfile( InterestProfileType interestProfileType );
 
 	public Map<String, InterestProfile> getInterestProfileMap();
 }
