4ce1b2d1b04647a963db63bb8d8f57ecdf41fed5
Sigit Nugraha <sigit.nugraha@gesis.org>
2016-01-10 19:07:55 +0100

    Update Circle and User DAO

--- src/main/java/de/rwth/i9/palm/persistence/CircleDAO.java
+++ src/main/java/de/rwth/i9/palm/persistence/CircleDAO.java
@@ -1,8 +1,21 @@
 package de.rwth.i9.palm.persistence;
 
+import java.util.Map;
+
+import de.rwth.i9.palm.model.Author;
 import de.rwth.i9.palm.model.Circle;
 
 public interface CircleDAO extends GenericDAO<Circle>, InstantiableDAO
 {
-
+	/**
+	 * Get circle from given query in pagination
+	 * 
+	 * @param query
+	 * @param creator
+	 * @param pageNo
+	 * @param maxResult
+	 * @param orderBy
+	 * @return
+	 */
+	public Map<String, Object> getCircleWithPaging( String query, Author creator, int pageNo, int maxResult, String orderBy );
 }
