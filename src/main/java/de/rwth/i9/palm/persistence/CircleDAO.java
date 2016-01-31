e30c3d0818ce0b46905f23e1899266d2ee131921
Sigit Nugraha <sigit.nugraha@gesis.org>
2016-02-01 00:25:07 +0100

    Add some interfaces on CircleDAO

--- src/main/java/de/rwth/i9/palm/persistence/CircleDAO.java
+++ src/main/java/de/rwth/i9/palm/persistence/CircleDAO.java
@@ -8,6 +8,13 @@ import de.rwth.i9.palm.model.Circle;
 public interface CircleDAO extends GenericDAO<Circle>, InstantiableDAO
 {
 	/**
+	 * Trigger batch indexing using Hibernate search powered by Lucene
+	 * 
+	 * @throws InterruptedException
+	 */
+	public void doReindexing() throws InterruptedException;
+
+	/**
 	 * Get circle from given query in pagination
 	 * 
 	 * @param query
@@ -18,4 +25,16 @@ public interface CircleDAO extends GenericDAO<Circle>, InstantiableDAO
 	 * @return
 	 */
 	public Map<String, Object> getCircleWithPaging( String query, Author creator, int pageNo, int maxResult, String orderBy );
+
+	/**
+	 * Get circle from given query in pagination
+	 * 
+	 * @param query
+	 * @param creator
+	 * @param pageNo
+	 * @param maxResult
+	 * @param orderBy
+	 * @return
+	 */
+	public Map<String, Object> getCircleFullTextSearchWithPaging( String query, Author creator, int pageNo, int maxResult, String orderBy );
 }
